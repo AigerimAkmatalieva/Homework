24d206b9cd433701a4490c31f0329cbf4c84c240
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2022-06-07 13:54:54 +0600

    added homework

---
+++ src/components/Example/Example.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+function Example() {
+  const [items, setItems] = useState([]);
+  const [item, setItem] = useState("");
+
+  function Add(event) {
+    event.preventDefault();
+    setItems([
+      ...items,
+      {
+        name: item,
+      },
+    ]);
+    setItem("");
+  }
+  function Delete() {
+    setItems(item.name.remove());
+  }
+
+  return (
+    <>
+      <form onSubmit={Add}>
+        <input
+          name="item"
+          type="text"
+          value={item}
+          placeholder="Type hire"
+          onChange={(item) => setItem(item.target.value)}
+        />
+         <button onClick={Add}>Add</button>
+      </form>
+      <ul>
+        {items.map((item) => (
+          <ol>
+            <li>
+            {item.name}
+            <button onClick={Delete}>-</button>
+            </li>
+          </ol>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default Example;
